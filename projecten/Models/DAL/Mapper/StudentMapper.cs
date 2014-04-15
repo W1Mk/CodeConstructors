96d4701f12d2ccab7c1c096b9c5b79a104b30d3d
Niels De Pauw <depauwniels@hotmail.com>
2014-04-15 19:00:14 +0200

    commit niels

---
+++ projecten/Models/DAL/Mapper/StudentMapper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Web;
+using projecten.Models.Domain;
+using Newtonsoft.Json;
+
+namespace projecten.Models.DAL.Mapper
+{
+    public class StudentMapper : EntityTypeConfiguration<Student>
+    {
+        public StudentMapper()
+        {
+            //Table
+            ToTable("student");
+            //properties
+            
+            Property(s => s.id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+            Property(s => s.naam).IsRequired().HasMaxLength(100);
+            Property(s => s.email).IsRequired();
+            Property(s => s.wachtwoord).IsRequired();
+        }
+	}
+}
